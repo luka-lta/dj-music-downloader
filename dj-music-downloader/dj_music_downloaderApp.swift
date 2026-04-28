6b154b625c847a63c0c0ab79dea1382c9cf992d7
luka-lta <luka@luka-lta.dev>
2026-04-28 20:38:47 +0200

    Initial Commit

---
+++ dj-music-downloader/dj_music_downloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  dj_music_downloaderApp.swift
+//  dj-music-downloader
+//
+//  Created by Luka on 28.04.26.
+//
+
+import SwiftUI
+
+@main
+struct dj_music_downloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
